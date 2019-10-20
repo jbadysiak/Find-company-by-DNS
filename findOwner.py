91c920d26e32a1f9a0d06bc9d474f96024190a34
badysiak.jb <badysiak.jb@pg.com>
2019-10-20 23:28:22 +0200

    Finding the owner of the domain

---
+++ findOwner.py
@@ -0,0 +1,37 @@
+import json
+import sys
+from http import client
+
+
+def connect_to_api(domain, request):
+    connection = client.HTTPSConnection(domain)
+    connection.request("GET", request)
+    response = connection.getresponse().read().decode("utf-8")
+    return json.loads(response)
+
+
+def check_dns(name):
+    json_response = connect_to_api("dns.google.com", "/resolve?name=" + name + "&type=A")
+    result = []
+    for answer in json_response.get('Answer'):
+        data = answer.get('data')
+        splitted_data = data.split('.')
+        if splitted_data[0].isnumeric():
+            result.append(data)
+    return result
+
+
+def check_ip(ip_list, api_key):
+    list_organization = []
+    for ip in ip_list:
+        json_response = connect_to_api("www.whoisxmlapi.com", "/whoisserver/WhoisService?apiKey=" + api_key + "&domainName=" + ip + "&outputFormat=JSON")
+        list_organization.append(json_response.get("WhoisRecord").get("registryData").get("registrant").get("organization"))
+
+    return list_organization
+
+
+page = sys.argv[1]
+api_key = sys.argv[2]
+ip_list = check_dns(page)
+organizations = check_ip(ip_list, api_key)
+print(organizations)
